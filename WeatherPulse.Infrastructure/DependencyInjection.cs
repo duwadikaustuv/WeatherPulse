f4239d60beab7a2f5dfd349d5326ef8caf7c5050
duwadikaustuv <kaustuv.duwadi@gmail.com>
2026-06-22 23:43:33 +0545

    Add DependencyInjection class for service registration and configuration

---
+++ WeatherPulse.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,41 @@
+﻿using Hangfire;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Polly.Extensions.Http;
+using WeatherPulse.Application.Interfaces;
+using WeatherPulse.Infrastructure.Clients;
+using WeatherPulse.Infrastructure.Policies;
+
+namespace WeatherPulse.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Register Settings
+        services.Configure<OpenWeatherSettings>(configuration.GetSection("OpenWeather"));
+
+        // Register HttpClient with Polly Retry Policy
+        services.AddHttpClient<IWeatherClient, OpenWeatherClient>(client =>
+        {
+            client.Timeout = TimeSpan.FromSeconds(30);
+        }).AddPolicyHandler(RetryPolicies.GetExponentialRetryPolicy()); // Applies 2s, 4s, 8s retries
+
+        // Redis Distributed Cache (matches SRS Redis TTL)
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = configuration.GetConnectionString("Redis");
+            options.InstanceName = "WeatherPulse_";
+        });
+
+        // Hangfire (for background jobs)
+        services.AddHangfire(config => config
+            .SetDataCompatibilityLevel(Hangfire.Common.CompatibilityLevel.Version_180)
+            .UseSimpleAssemblyNameTypeSerializer()
+            .UseRecommendedSerializerSettings()
+            .UsePostgreSqlStorage(configuration.GetConnectionString("HangfireDB")));
+        services.AddHangfireServer();
+
+        return services;
+    }
+}
