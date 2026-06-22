7b4857264bda928edae61c461fad87e4ef1c9c41
duwadikaustuv <kaustuv.duwadi@gmail.com>
2026-06-22 23:40:15 +0545

    Add WeatherResponseDto and IWeatherClient interface for weather data handling

---
+++ WeatherPulse.Application/DTOs/WeatherResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherPulse.Application.DTOs;
+
+public class WeatherResponseDto
+{
+    public string City { get; set; }
+    public double TempC { get; set; }
+    public int Aqi { get; set; }
+    public double Uv { get; set; }
+    public int OutdoorScore { get; set; }
+    public string Advice { get; set; } // "Great day for a run!"
+}
---
+++ WeatherPulse.Application/Interfaces/IWeatherClient.cs
@@ -0,0 +1,8 @@
+﻿using WeatherPulse.Application.DTOs;
+
+namespace WeatherPulse.Application.Interfaces;
+
+public interface IWeatherClient
+{
+    Task<WeatherResponseDto> GetWeatherAsync(string city, CancellationToken ct);
+}
